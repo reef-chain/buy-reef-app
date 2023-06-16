a383e8d0600b269250a8c800717799a8de6ee34f
Anukul Pandey <anukul.030601@gmail.com>
2023-06-16 16:14:17 +0530

    fix: overflow in modal

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -7,7 +7,7 @@ interface Children {
 interface ModalProps extends Children {
   id?: string;
   displayModal?: boolean;
-  setDisplayModal? : (displayModal:boolean)=>void;
+  setDisplayModal?: (displayModal: boolean) => void;
 }
 
 export const Modal: React.FC<ModalProps> = ({
@@ -20,7 +20,7 @@ export const Modal: React.FC<ModalProps> = ({
 
   useEffect(() => {
     const handleOutsideClick = (event: MouseEvent) => {
-      if (modalRef.current && !modalRef.current.contains(event.target as Node) && setDisplayModal!) {   
+      if (modalRef.current && !modalRef.current.contains(event.target as Node) && setDisplayModal) {
         setDisplayModal(false);
       }
     };
@@ -43,10 +43,35 @@ export const Modal: React.FC<ModalProps> = ({
       tabIndex={-1}
       aria-labelledby={id}
       aria-hidden="true"
-      style={{ display: 'block', backgroundColor: 'rgba(0, 0, 0, 0.5)', position: 'fixed', top: 0, left: 0, right: 0, bottom: 0, zIndex: 999 }}
+      style={{
+        display: 'block',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        zIndex: 999
+      }}
     >
-      <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100%' }}>
-        <div ref={modalRef} style={{ backgroundColor: '#eeebf6', borderRadius: '8px', padding: '20px' }}>
+      <div
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+          height: '100%'
+        }}
+      >
+        <div
+          ref={modalRef}
+          style={{
+            backgroundColor: '#eeebf6',
+            borderRadius: '8px',
+            padding: '20px',
+            height: '400px',
+            overflow: 'auto'
+          }}
+        >
           {children}
         </div>
       </div>
@@ -55,20 +80,14 @@ export const Modal: React.FC<ModalProps> = ({
 };
 
 export const Title: React.FC<Children> = ({ children }): JSX.Element => (
-  <div style={{'fontSize':'24px'}}>{children}</div>
+  <div style={{ fontSize: '24px' }}>{children}</div>
 );
 
-export const ModalHeader: React.FC<Children> = ({ children }): JSX.Element => (
-  <div>{children}</div>
-);
+export const ModalHeader: React.FC<Children> = ({ children }): JSX.Element => <div>{children}</div>;
 
-export const ModalBody: React.FC<Children> = ({ children }): JSX.Element => (
-  <div>{children}</div>
-);
+export const ModalBody: React.FC<Children> = ({ children }): JSX.Element => <div>{children}</div>;
 
-export const ModalFooter: React.FC<Children> = ({ children }): JSX.Element => (
-  <div>{children}</div>
-);
+export const ModalFooter: React.FC<Children> = ({ children }): JSX.Element => <div>{children}</div>;
 
 interface ModalCloseProps {
   onClick?: () => void;
@@ -79,7 +98,7 @@ interface ModalCloseProps {
 export const ModalClose: React.FC<ModalCloseProps> = ({
   children,
   onClick = () => {},
-  className,
+  className
 }): JSX.Element => (
   <button
     type="button"
@@ -87,7 +106,14 @@ export const ModalClose: React.FC<ModalCloseProps> = ({
     onClick={onClick}
     data-bs-dismiss="modal"
     aria-label="Close"
-    style={{ position: 'absolute', top: '10px', right: '10px', background: 'none', border: 'none', cursor: 'pointer' }}
+    style={{
+      position: 'absolute',
+      top: '10px',
+      right: '10px',
+      background: 'none',
+      border: 'none',
+      cursor: 'pointer'
+    }}
   >
     {children}
   </button>
@@ -103,7 +129,7 @@ export const OpenModalButton: React.FC<OpenModalButtonProps> = ({
   children,
   id = 'open-modal-button',
   disabled,
-  className,
+  className
 }): JSX.Element => (
   <button
     type="button"
@@ -128,7 +154,7 @@ const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
   title,
   confirmFun,
   confirmBtnLabel = 'Confirm',
-  children,
+  children
 }): JSX.Element => (
   <Modal id={id} displayModal={true}>
     <ModalHeader>
