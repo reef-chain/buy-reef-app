5128a49d8961fd865993e5dce348cb0d41b7211a
Anukul Pandey <anukul.030601@gmail.com>
2023-06-19 14:09:28 +0530

    feat: input field for address

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,14 @@ import Loader from './components/Loader/Loader';
 import GradientButton from './components/GradientButton/GradientButton';
 import Navbar from './components/Navbar/Navbar';
 import TextButton from './components/TextButton/TextButton';
-
-interface Status {
-  inProgress: boolean;
-  message?: string;
-}
+import InputField from './components/InputField/InputField';
 
 const App = (): JSX.Element => {
 
   return (
     <div>
       <Navbar/>
-      
+      <InputField/>  
     </div>
   );
 };
---
+++ src/components/InputField/InputField.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './styles.css'
+
+function InputField() {
+  return (
+    <div>
+        <input type="text" className='address-input-field' placeholder='Wallet Address'/>
+    </div>
+  )
+}
+
+export default InputField
