99f06800eb171fceba8a4a9599c2b956aac51b7e
Anukul Pandey <anukul.030601@gmail.com>
2023-06-19 21:38:59 +0530

    feat: text input

--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -7,8 +7,8 @@ interface Props{
 
 function InputField({setAddress}:Props) {
   return (
-    <div>
-        <input type="text" className='address-input-field' placeholder='Wallet Address' onChange={(e)=>setAddress(e.target.value)}/>
+    <div style={{display:'flex'}}>
+        <input type="text" className='address-input-field' placeholder='Wallet Address' style={{flex:1}} onChange={(e)=>setAddress(e.target.value)}/>
     </div>
   )
 }
